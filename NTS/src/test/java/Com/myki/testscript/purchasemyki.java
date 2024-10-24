049172b8d7fe8c7cc26be692dddfd3964896fc0e
mayak <mayak@Maya>
2024-10-24 11:39:28 +0530

    get myki

--- NTS/src/test/java/Com/myki/testscript/purchasemyki.java
+++ NTS/src/test/java/Com/myki/testscript/purchasemyki.java
@@ -1,5 +1,19 @@
 package Com.myki.testscript;
 
-public class purchasemyki {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class purchasemyki {
+	@Test
+	public static void test() {
+		
+WebDriver driver= new ChromeDriver();
+driver.get("https://www.ptv.vic.gov.au/tickets/myki");
+String text = driver.findElement(By.xpath("//h1[text()='myki']")).getText();
+Reporter.log(text);
+
+}
 }
